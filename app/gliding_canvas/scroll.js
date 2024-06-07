26616cc314829038d59b01a9d8300d0b584474a4
malik <malikkotb@icloud.com>
2024-06-07 19:07:22 +0200

    1

--- app/gliding_canvas/scroll.js
+++
@@ -1,8 +0,0 @@
-const gallery = document.getElementById("gallery");
-
-window.addEventListener("scroll", () => {
-  const scrollX = window.scrollX;
-  const scrollY = window.scrollY;
-
-  gallery.style.transform = `translate(${-scrollX}px, ${-scrollY}px)`;
-});
